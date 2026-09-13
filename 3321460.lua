dec99501b85333350ffb26e4c9542eb49c1fa28a
fyisdy <fylsdy@foxmail.com>
2026-09-14 02:00:25 +0800

    Add files via upload

---
+++ 3321460.lua
@@ -0,0 +1,29 @@
+--====================================================================================
+-- APPID:3321460;Crimson Desert
+--====================================================================================
+addappid(3321460,1,"dc014470e47064263f3003feb526a87cc64784292e2a608455d18fa70ec2c1e0")
+addappid(3321461,0,"e2e570c84eb8b4cfe9c20268cd30b84f8c1c7c076691c92ae8c5ba7b510b8a95")
+addappid(3321462,0,"6eb3bfe7d16b9eb6441dba4f7d18b341fe83ab2f1e9385d042ce622f38a600cb")
+--addappid(4783050 [No size]	Windows 64-bitUnusedDLC 4783050SteamDB Unknown App 4783050 ) 	
+--addappid(3321469 [No size]	Windows 64-bit 	)
+--addappid(3321463 [NEED OWNER SHARE]	Windows 64-bitJapanese 	1.69 GiB 	1.68 GiB
+--addappid(3321464 [NEED OWNER SHARE]	Windows 64-bitSpanish - Spain 	1.81 GiB 	1.80 GiB
+--addappid(3321465 [NEED OWNER SHARE]	Windows 64-bitFrench 	1.55 GiB 	1.55 GiB
+--addappid(3321466 [NEED OWNER SHARE]	Windows 64-bitGerman 	1.83 GiB 	1.80 GiB
+--addappid(3321467 [NEED OWNER SHARE]	Windows 64-bitPortuguese - Brazil 	1.67 GiB 	1.66 GiB
+--====================================================================================
+-- APPID:4572870;Crimson Desert Original Soundtrack Volume 1
+--====================================================================================
+addappid(4572871,0,"fd5557767303d9a63b0b31b6a22cd1368e5d82a692e64d041890100ea0c330c4")
+addappid(4572872,0,"e071efcdac5de6bf1b6620a3110cb3ab7799943ee634eed2970db8732eeb817e")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(4572870)
+addtoken(4024630,"17135069641874014023")
