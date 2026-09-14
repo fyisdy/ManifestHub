1a0abf38c5338e2ed87678b0a570228cae332e44
fyisdy <fylsdy@foxmail.com>
2026-09-14 18:03:20 +0800

    Update manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -1,4 +1,4 @@
-﻿function fetch_manifest_code_disabled(gid)
+﻿function fetch_manifest_code(gid)
     local body, st = http_get("https://procurement-viii-sara-lang.trycloudflare.com/manifest/" .. gid)
     if st == 200 and body and body:match("^%d+$") then
         return body
