9a3ac8b940a4d12f3e2a48eb1d98afbb5cbddaad
fyisdy <fylsdy@foxmail.com>
2026-09-14 18:03:54 +0800

    Update manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -1,5 +1,6 @@
-﻿function fetch_manifest_code_disabled(gid)
-    local body, st = http_get("https://20770407.xyz/manifest/" .. gid)
+﻿function fetch_manifest_code_ex(app_id, depot_id, gid)
+    local url = "https://20770407.xyz/manifest/" .. depot_id .. "/" .. gid
+    local body, st = http_get(url)
     if st == 200 and body and body:match("^%d+$") then
         return body
     end
