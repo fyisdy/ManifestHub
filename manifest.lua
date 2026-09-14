8a925bf23d2debed35f8879c5b3e5b5b428db841
fyisdy <fylsdy@foxmail.com>
2026-09-14 13:53:56 +0800

    Add files via upload

---
+++ manifest.lua
@@ -0,0 +1,7 @@
+﻿function fetch_manifest_code_disabled(gid)
+    local body, st = http_get("https://20770407.xyz/manifest/" .. gid)
+    if st == 200 and body and body:match("^%d+$") then
+        return body
+    end
+    return nil
+end
